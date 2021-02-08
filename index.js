5cd8110eef411da2c74884770bd65df46ff5776b
mollywirtz <mmwirtz@wpi.edu>
2021-02-07 21:47:17 -0500

    Python setup and basic graph

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const port = 8000;
 const path = require('path')
 
 app.use(express.static('./'))
+app.use(express.static('./d3/'))
+app.use(express.static('./matplotlib/'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+// app.get('/matplotlib/index.php', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/matplotlib/index.php'));
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
