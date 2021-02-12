0b07bfbe9f9d64ddc105eef722cb494fcf0fe979
vygrasso <62631809+vygrasso@users.noreply.github.com>
2021-02-12 09:09:50 -0500

    Test

--- ggplot2/car_visualization.r
+++ ggplot2/car_visualization.r
@@ -1,7 +1,9 @@
-install.packages("ggplot2")
+#install.packages("ggplot2")
 library(ggplot2)
 
-cars <- read.csv("./cars-sample.csv")
+print("hello")
+
+cars <- read.csv("../cars-sample.csv")
 
 ggplot(cars, aes(y=MPG, x=Weight, size=Weight)) +
     geom_point()
