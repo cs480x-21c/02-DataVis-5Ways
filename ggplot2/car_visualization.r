71125f9f03fa7c7abfd3a481e806d2874118bbfb
vygrasso <62631809+vygrasso@users.noreply.github.com>
2021-02-12 02:25:58 -0500

    Update car_visualization.r

--- ggplot2/car_visualization.r
+++ ggplot2/car_visualization.r
@@ -1,9 +1,9 @@
 install.packages("ggplot2")
 library(ggplot2)
 
-cars <- read.csv("../cars-sample.csv")
+cars <- read.csv("./cars-sample.csv")
 
 ggplot(diabetes, aes(y=glyhb, x=age, size=weight, color=frame)) +
     geom_point()
 
-ggsave("../img/ggplotcarsvisualization.png")
+#ggsave("../img/ggplotcarsvisualization.png")
