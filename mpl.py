72999a2643518b058a3089ea81a60c4b573bc739
Yoni <yweiner@wpi.edu>
2021-02-12 16:49:40 -0500

    add ggplot

--- mpl.py
+++ mpl.py
@@ -1,5 +1,14 @@
 import pandas as pd
+from plotnine import ggplot, aes, geom_point
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('cars-sample.csv')
 print(df)
+#
+# ax1 = df.plot.scatter(x='Weight',
+#                       y='MPG',
+#                       c='Manufacturer',
+#                       colormap='viridis')
+(
+ggplot(aes(x='Weight', y='MPG', color='Manufacturer'), data=df) + geom_point()
+)
