2d97051f4810eabab11516b495530dea1305195e
Isabel Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2021-02-12 23:33:07 -0500

    fixing altair

--- python/altair-vis.py
+++ python/altair-vis.py
@@ -8,11 +8,11 @@ data = pandas.read_csv('cars-sample.csv')
 xdata = data['Weight']
 ydata = data['MPG']
 manufacturers=data['Manufacturer']
-colors = ["lightcoral","tab:green","green","cornflowerblue","tab:pink"]
+colors = ["lightcoral","olive","green","cornflowerblue","fuchsia"]
 man_types = ["bmw","ford","honda","mercedes","toyota"]
 
 dictionary = pandas.DataFrame(dict(Weight=xdata,MPG=ydata,Manufacturer = manufacturers))
 
 chart = alt.Chart(dictionary).mark_circle(opacity=0.5).encode(x=alt.X('Weight', scale=alt.Scale(domain=(1500, 5000))),y='MPG', size = 'Weight:N',color= alt.Color('Manufacturer', scale=alt.Scale(domain=man_types, range=colors)))
 
-chart.save('chart.html')
+chart.save('altair.html')
